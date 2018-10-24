9536d5461ee413e7b016087899569d99e79417a1
seu <seu@panopticon.re>
2018-10-25 01:09:45 +0200

    better Frame/Batch API

--- lib/frame.ex
+++ lib/frame.ex
@@ -1,92 +1,49 @@
 defmodule Frame do
   defstruct type: :nil, data: :nil
 
-  def parse(frm), do: parse(frm, "", :outside)
-  defp parse("." <> cdr, frm, :outside), do: {%Frame{ type: :text, data: frm }, cdr}
-  defp parse("", frm, _), do: {%Frame{ type: :text, data: frm }, ""}
-  defp parse(cdr, frm, state) do
-    char = String.first(cdr)
-    case {char, state} do
-      {"^", :outside} -> parse(String.slice(cdr, 1..-1), frm <> char, :number)
-      {"'", :outside} -> parse(String.slice(cdr, 1..-1), frm <> char, :inside)
-      {"'", :escaped} -> parse(String.slice(cdr, 1..-1), frm <> char, :inside)
-      {"'", :inside} -> parse(String.slice(cdr, 1..-1), frm <> char, :outside)
-      {"\\", :inside} -> parse(String.slice(cdr, 1..-1), frm <> char, :escaped)
-      {_, :escaped} -> parse(String.slice(cdr, 1..-1), frm <> char, :inside)
-      {".", :outside} -> {%Frame{ type: :text, data: frm }, String.slice(cdr, 1..-1)}
-      {c, :number} ->
-        is_num_char = ?c in ?0..?9 or c == "-" or c == "+" or c
-        if is_num_char do
-          parse(String.slice(cdr, 1..-1), frm <> char, :number)
+  def parse(str), do: parse(str, [])
+  defp parse("", ops), do: {:ok, ops, ""}
+  defp parse("." <> cdr, ops), do: {:ok, ops, cdr}
+  defp parse("\t" <> cdr, ops), do: parse(cdr, ops)
+  defp parse("\n" <> cdr, ops), do: parse(cdr, ops)
+  defp parse("\r" <> cdr, ops), do: parse(cdr, ops)
+  defp parse("\v" <> cdr, ops), do: parse(cdr, ops)
+  defp parse(" " <> cdr, ops), do: parse(cdr, ops)
+  defp parse(str, ops) do
+    prev = case ops do
+      [] ->
+        %Op{
+          type: UUID.zero(), event: UUID.zero(),
+          object: UUID.zero(), location: UUID.zero()
+        }
+      ops -> List.last(ops)
+    end
+
+    case Op.parse(str, prev) do
+      {:ok, {op, cdr}} ->
+        if ops == [] do
+          parse(cdr, [op])
         else
-          parse(String.slice(cdr, 1..-1), frm <> char, :outside)
+          prev = List.last(ops)
+          %Op{ term: prev_term } = prev
+          %Op{ term: term } = op
+
+          if prev_term == :raw or term != :reduced do
+            {:ok, ops, str}
+          else
+            parse(cdr, ops ++ [op])
+          end
         end
-      {_, _} -> parse(String.slice(cdr, 1..-1), frm <> char, state)
+
+      err = {:error, _} -> err
     end
   end
 
-  def ops(%Frame{ type: :text, data: str }) do
-    init = %Op{ type: :nil, event: :nil, object: :nil, location: UUID.zero() }
-    Stream.unfold({str, init}, fn
-      {"", _} -> :nil
-      {str, prev} ->
-        case Op.parse(str, prev) do
-          {:ok, {op, cdr}} ->
-            cdr = String.trim_leading(cdr)
-            {next_prev, cdr} = case cdr do
-              "." <> _ -> {init, String.slice(cdr, 1..-1)}
-              "" -> {init, ""}
-              _ -> case op do
-                %Op{ term: :reduced } -> {op, cdr}
-                %Op{ term: :header } -> {op, cdr}
-                %Op{ term: :raw } -> {init, cdr}
-                %Op{ term: :query } -> {op, cdr}
-              end
-            end
-
-            {{:ok, op}, {cdr, next_prev}}
-
-          {:error, msg} -> {{:error, msg}, {"", prev}}
-        end
-    end)
-  end
-
-  def ops!(frame) do
-    ops(frame) |> Stream.map(fn
-      {:ok, op} -> op
+  def parse!(str) do
+    case parse(str) do
+      {:ok, ops, cdr} -> {ops, cdr}
       {:error, msg} -> raise msg
-    end)
-  end
-
-  def chunks(frm = %Frame{}) do
-    Frame.ops(frm) |> Stream.chunk_while([], fn
-      {:ok, op = %Op{ term: :raw }}, [] ->
-        {:cont, [op], []}
-      {:ok, op = %Op{ term: :raw }}, ops ->
-        {:cont, ops, [op]}
-      {:ok, op = %Op{ term: :header }}, [] ->
-        {:cont, [op]}
-      {:ok, op = %Op{ term: :header }}, ops ->
-        {:cont, ops, [op]}
-      {:ok, op = %Op{ term: :query }}, [] ->
-        {:cont, [op]}
-      {:ok, op = %Op{ term: :query }}, ops ->
-        {:cont, ops, [op]}
-      {:ok, %Op{ term: :reduced }}, [] ->
-        {:halt, {:error , "reduced op w/o preceeding header"}}
-      {:ok, op = %Op{ term: :reduced }}, ops = [%Op{ term: :header } | _] ->
-        {:cont, ops ++ [op]}
-      {:ok, op = %Op{ term: :reduced }}, ops = [%Op{ term: :query } | _] ->
-        {:cont, ops ++ [op]}
-      {:ok, %Op{ term: :reduced }}, [%Op{ term: :raw }] ->
-        {:halt, {:error , "reduced op w/o preceeding header"}}
-      {:error, msg}, _ ->
-        {:halt, {:error , msg}}
-      end,
-    fn
-      [] -> {:cont, []}
-      ops -> {:cont, ops, []}
-    end)
+    end
   end
 end
 
@@ -100,30 +57,20 @@ defimpl String.Chars, for: Frame do
 end
 
 defmodule Batch do
-  defstruct frames: []
-
   def parse(str), do: parse(str, [])
-  def parse(str, batch) do
-    str = String.trim_leading(str)
-    if str == "" do
-      %Batch{ frames: batch }
-    else
-      {frame, cdr} = Frame.parse(str)
-      parse(cdr, batch ++ [frame])
+  defp parse(str, batch) do
+    case Frame.parse(str) do
+      {:ok, [], cdr} -> {:ok, batch}
+      {:ok, ops, ""} -> {:ok, batch ++ [ops]}
+      {:ok, ops, cdr} -> parse(cdr, batch ++ [ops])
+      err = {:error, _} -> err
     end
   end
 
-  def ops(%Batch{ frames: frames  }) do
-    Enum.reduce(frames, Stream.map([], &(&1)), fn frm, strm ->
-      Stream.concat(strm, Frame.ops(frm))
-    end)
-  end
-
-  def ops!(batch) do
-    ops(batch)
-    |> Stream.map(fn
-      {:ok, op} -> op
+  def parse!(str) do
+    case parse(str) do
+      {:ok, ops} -> ops
       {:error, msg} -> raise msg
-    end)
+    end
   end
 end
--- test/frame_test.exs
+++ test/frame_test.exs
@@ -2,24 +2,41 @@ defmodule FrameTest do
   use ExUnit.Case
   doctest Frame
 
-  test "decode text" do
+  test "basic decode" do
     txt = "  \n*rga#1UQ8p+bart@1UQ8yk+lisa:0!\n    @(s+bart'H'@[r'e'@(t'l'@[T'l'@[i'o'\n    @(w+lisa' '@(x'w'@(y'o'@[1'r'@{a'l'@[2'd'@[k'!'"
-    Frame.parse(txt)
+    {:ok, frame, cdr} = Frame.parse(txt)
+
+    assert cdr == ""
+    assert length(frame) == 13
   end
 
-  test "ron-tests 01-lww-basic" do
-    t1 = "*lww#test1!\n    *lww#test1@time:a'A';"
-    t2 = "*lww#test2@1:0!:a'A'\n *lww#test2@2:b'B';"
-    t3 = "*lww#test3@1:a'A1';*lww#test3@2:a'A2';"
-    t4 = "*lww#test4@2:1!     :a  'A1'    :b  'B1'    :c  'C1'*lww#test4@3:1!     :a  'A2'    :b  'B2'"
-    t5 = "*lww#array@1:0!    :0%0 =0,      :)1%0 =-1*lww#array@2:0!     :0%)1 '1',      :)1%0 =1,      :)1%)1 =65536"
-    t6 = "*lww#weird@0:0!*lww#weird@1 :longString 'While classic databases score 0 on the ACID\\' scale, I should probably reserve the value of -1 for one data sync system based on Operational Transforms.\\n Because of the way its OT mechanics worked, even minor glitches messed up the entire database through offset corruption. That was probably the worst case I observed in the wild. Some may build on quicksand, others need solid bedrock… but that system needed a diamond plate to stay still.' ;*lww#weird@2 :pi ^3.141592653589793 ;*lww#weird@3 :minus =-9223372036854775808 ;"
-    t7 = "    *lww#raw@1:one=1;@2:two^2.0:three'три'"
-    txt = [t1,t2,t3,t4,t5,t6,t7]
+  test "decode first" do
+    txt = "*lww#test1!\n    *lww#test1@time:a'A';"
+    {:ok, frame, cdr} = Frame.parse(txt)
 
-    Enum.each(txt, fn txt ->
-      {frm, _} = Frame.parse(txt)
-      frm |> Frame.ops |> Stream.run
-    end)
+    assert cdr == "*lww#test1@time:a'A';"
+    assert length(frame) == 1
+  end
+
+  test "decode nothing" do
+   {:ok, frame, cdr} = Frame.parse("")
+    assert cdr == ""
+    assert length(frame) == 0
+
+    {:ok, frame, cdr} = Frame.parse(".")
+    assert cdr == ""
+    assert length(frame) == 0
+  end
+
+  test "decode error" do
+   {:error, _} = Frame.parse("XX")
+  end
+
+  test "decode batch" do
+    txt = "*lww#test1!\n    *lww#test1@time:a'A';"
+    {:ok, [f1 | [f2 | []]]} = Batch.parse(txt)
+
+    assert length(f1) == 1
+    assert length(f2) == 1
   end
 end
