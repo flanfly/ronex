e3e9fb15b020eb89782fedb12c57ea696c44921b
seu <seu@panopticon.re>
2018-10-25 12:12:48 +0200

    fix LWW & tests

--- lib/crdt/lww.ex
+++ lib/crdt/lww.ex
@@ -1,32 +1,30 @@
 defmodule LWW do
-  def reduce([], []), do: []
-  def reduce(frame) do
-    [head | _] = frame
-    latest = Enum.at(frame, -1).event
+  def reduce(_, []), do: []
+  def reduce(state, update) do
+    [head | _] = state
+    %Op{ event: latest } = Enum.at(update, -1)
     spec = %Op{ head | event: latest, location: UUID.zero(), term: :header }
+    body = Enum.filter(state ++ update, fn %Op{ term: term } ->
+      term != :header and term != :query
+    end)
+    |> Enum.sort(fn a,b ->
+      if a.location == b.location do
+        a.event > b.event
+      else
+        a.location < b.location
+      end
+    end)
+    |> Enum.chunk_by(fn %Op{ location: loc } -> loc end)
+    |> Enum.map(fn [op | _] -> %Op{ op | term: :reduced } end)
 
-    [spec |
-      frame
-      |> Enum.filter(fn %Op{ term: term } ->
-        term != :header and term != :query
-      end)
-      |> Enum.sort(fn a,b ->
-        if a.location == b.location do
-          a.event > b.event
-        else
-          a.location < b.location
-        end
-      end)
-      |> Enum.chunk_by(fn %Op{ location: loc } -> loc end)
-      |> Enum.map(fn [op | _] -> op end)
-    ]
+    [spec | body]
   end
 
   def map([]), do: %{}
   def map(state) do
     Enum.reduce(state, %{}, fn
       %Op{ term: :header }, acc -> acc
-      %Op{ location: loc, atoms: [] }, acc -> acc
+      %Op{ atoms: [] }, acc -> acc
       %Op{ location: loc, atoms: [val] }, acc when is_float(val) ->
         Map.put(acc, loc, Float.round(val, 5))
       %Op{ location: loc, atoms: [val] }, acc ->
--- test/lww_test.exs
+++ test/lww_test.exs
@@ -3,42 +3,54 @@ defmodule LWWTest do
   doctest LWW
 
   test "ron-tests 01-lww-basic" do
-    in1 = "*lww#test1!\n.*lww#test1@time:a'A';"
+    st1 = "*lww#test1!"
+    up1 = "*lww#test1@time:a'A';"
     out1 = "*lww#test1@time:0!\n      :a      'A' ,"
-    test_pair(in1, out1)
+    test_lww(st1, up1, out1)
 
-    in2 = "*lww#test2@1:0!:a'A'\n.*lww#test2@2:b'B';"
+    st2 = "*lww#test2@1:0!:a'A'"
+    up2 = "*lww#test2@2:b'B';"
     out2 = "*lww#test2@2:0!\n    @1  :a      'A' ,\n    @2  :b      'B' ,"
-    test_pair(in2, out2)
+    test_lww(st2, up2, out2)
 
-    in3 = "*lww#test3@1:a'A1';\n*lww#test3@2:a'A2';"
+    st3 = "*lww#test3@1:a'A1';"
+    up3 = "*lww#test3@2:a'A2';"
     out3 = "*lww#test3@2:1!\n        :a      'A2' ,"
-    test_pair(in3, out3)
+    test_lww(st3, up3, out3)
 
-    in4 = "*lww#test4@2:1!\n    :a  'A1'\n    :b  'B1'\n    :c  'C1'\n*lww#test4@3:1! \n    :a  'A2'\n    :b  'B2'\n"
+    st4 = "*lww#test4@2:1!\n    :a  'A1'\n    :b  'B1'\n    :c  'C1'"
+    up4 = "*lww#test4@3:1! \n    :a  'A2'\n    :b  'B2'\n"
     out4 = "*lww#test4@3:1!\n        :a      'A2' ,\n        :b      'B2' ,\n    @2  :c      'C1' ,\n"
-    test_pair(in4, out4)
+    test_lww(st4, up4, out4)
 
-    in5 = "*lww#array@1:0!\n    :0%0 =0,  \n    :)1%0 =-1\n*lww#array@2:0! \n    :0%)1 '1',  \n    :)1%0 =1,  \n    :)1%)1 =65536\n"
+    st5 = "*lww#array@1:0!\n    :0%0 =0,  \n    :)1%0 =-1"
+    up5 = "*lww#array@2:0! \n    :0%)1 '1',  \n    :)1%0 =1,  \n    :)1%)1 =65536\n"
     out5 = "*lww#array@2:0!\n     @1  :0%0      =0  ,\n    @2  :0%0000000001    '1' ,\n        :0000000001%0    =1  ,\n        :0000000001%0000000001    =65536  ,"
-    test_pair(in5, out5)
+    test_lww(st5, up5, out5)
 
-    in6 = "*lww#weird@0:0!\n*lww#weird@1 :longString 'While classic databases score 0 on the ACID\\' scale, I should probably reserve the value of -1 for one data sync system based on Operational Transforms.\n Because of the way its OT mechanics worked, even minor glitches messed up the entire database through offset corruption. That was probably the worst case I observed in the wild. Some may build on quicksand, others need solid bedrock… but that system needed a diamond plate to stay still.' ;\n*lww#weird@2 :pi ^3.141592653589793 ;\n*lww#weird@3 :minus =-9223372036854775808 ;\n"
+    st6 = "*lww#weird@0:0!"
+    up6 = "*lww#weird@1 :longString 'While classic databases score 0 on the ACID\\' scale, I should probably reserve the value of -1 for one data sync system based on Operational Transforms.\n Because of the way its OT mechanics worked, even minor glitches messed up the entire database through offset corruption. That was probably the worst case I observed in the wild. Some may build on quicksand, others need solid bedrock… but that system needed a diamond plate to stay still.' ;\n*lww#weird@2 :pi ^3.141592653589793 ;\n*lww#weird@3 :minus =-9223372036854775808 ;\n"
     out6 = "*lww#weird@3:0!\n	@1 :longString 'While classic databases score 0 on the ACID\\' scale, I should probably reserve the value of -1 for one data sync system based on Operational Transforms.\n Because of the way its OT mechanics worked, even minor glitches messed up the entire database through offset corruption. That was probably the worst case I observed in the wild. Some may build on quicksand, others need solid bedrock… but that system needed a diamond plate to stay still.' ,\n	@3 :minus =-9223372036854775808 ,\n	@2 :pi ^3.141593e+00 ,"
-    test_pair(in6, out6)
+    test_lww(st6, up6, out6)
 
-    in7 = "*lww#raw@1:one=1;@2:two^2.0:three'три'"
+    st7 = "*lww#raw@1:one=1;"
+    up7 = "*lww#raw@2:two^2.0:three'три'"
     out7 = "*lww#raw@2:1!\n	@1 :one =1 ,\n	@2 :three 'три' ,\n  :two ^2.000000e+00 ,"
-    test_pair(in7, out7)
+    test_lww(st7, up7, out7)
   end
 
+  defp test_lww(state, updates, output) do
+    {state, _} = Frame.parse!(state)
+    updates = Batch.parse!(updates)
+    {output, _} = Frame.parse!(output)
 
-  defp test_pair(input, output) do
-    input = (input |> Batch.parse |> Batch.ops! |> Enum.to_list)
-    output = (output |> Batch.parse |> Batch.ops! |> Enum.to_list)
-    input = Set.reduce(input)
-    output = Set.reduce(output)
+    final = Enum.reduce(updates, state, fn update, state -> LWW.reduce(state, update) end)
+    final = LWW.map(final)
+    expected = LWW.map(output)
 
-    assert LWW.map(input) == LWW.map(output)
+    IO.inspect final
+    IO.inspect expected
+
+    assert final == expected
   end
 end
